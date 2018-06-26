8f656018bbaa49e11fc5732dfe96846bb85262a9
Dan <dmoisoff@gmail.com>
2018-06-26 15:23:18 -0400

    Finish Tic Tac Toe

--- tic-tac-toe/js/ttt-view.js
+++ tic-tac-toe/js/ttt-view.js
@@ -1,11 +1,54 @@
+require("./moveError.js");
+
 class View {
-  constructor(game, $el) {}
+  constructor(game, $el) {
+    this.game = game;
+    this.$el = $el;
+    this.setupBoard();
+    this.bindEvents();
+  }
 
-  bindEvents() {}
+  bindEvents() {
+    $('ul').on('click', 'li', (e) => {
+      const $square = $(e.currentTarget);
+      this.makeMove($square);
+    });
+  }
 
-  makeMove($square) {}
+  makeMove($square) {
+    try{
+      const currentPlayer = this.game.currentPlayer;
+      this.game.playMove($square.data('pos'));
+      $square.addClass(currentPlayer);
+      $square.text(currentPlayer);
+    }
+    catch(err){
+      alert(err.msg);
+    }
+    if(this.game.isOver()){
+      $('li').attr("style","background-color: white; color: red");
+      const winner = this.game.winner();
+      if (winner){
+        $(`li.${winner}`).attr("style", "background-color: green; color: white");
+        $('body').append(`<h2>You win, ${winner}!</h2>`);
+      } else {
+        $('body').append(`<h2>It's a draw!</h2>`);
+      }
+    }
+  }
 
-  setupBoard() {}
+  setupBoard() {
+    const board = this.game.board;
+    (this.$el).append('<ul class="board"></ul>');
+    for (let i = 0; i < 9; i++) {
+      const $square = $('<li class="square"></li>');
+      $square.data( 'pos', [ i % 3 , Math.floor(i / 3) ] );
+      $(".board").append($square);
+    }
+    
+  }
+  
+  
 }
 
 module.exports = View;
